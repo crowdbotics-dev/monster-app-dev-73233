e781a1f740e7889fe2f379770482bd769a8d012d
Ravinder sales <ravinder.y@crowdbotics.com>
2023-03-31 18:06:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled7/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled7 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      flex: 1,
+      flexDirection: 'column',
+      height: '100%',
+      padding: 10,
+      backgroundColor: '#f0f0f1'
+    }}>
+        
+          <View style={styles.column1}></View>
+          <View style={styles.column2}></View>
+          <View style={styles.column3}></View>
+        
+        </ScrollView>
+      </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  column1: {
+    flex: 1
+  },
+  column2: {
+    flex: 1,
+    opacity: 0.74,
+    backgroundColor: "#e14040"
+  },
+  column3: {
+    flex: 1
+  }
+});
+export default Untitled7;
